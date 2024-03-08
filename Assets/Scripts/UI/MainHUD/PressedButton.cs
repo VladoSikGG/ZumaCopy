9d4c09e60c360f59c722db22fb22475d8de5c2d9
RUDIK <vladislavrudakovskij@gmail.com>
2024-03-08 17:18:47 +0300

    AddMainScreen

--- Assets/Scripts/UI/MainHUD/PressedButton.cs
+++ Assets/Scripts/UI/MainHUD/PressedButton.cs
@@ -8,14 +8,24 @@ public class PressedButton : MonoBehaviour
 {
     [SerializeField] private GameObject _selfPanel;
     [SerializeField] private Sprite _notActiveView;
+    private AllPanels _allPanels;
 
     private void Awake()
     {
+        _allPanels = FindObjectOfType<AllPanels>();
         _notActiveView = GetComponent<Image>().sprite;
     }
 
     private void Update()
     {
         if (!_selfPanel.activeInHierarchy) GetComponent<Image>().sprite = _notActiveView;
+    }
+
+    public void OpenMenu()
+    {
+        foreach (var panel in _allPanels.panels)
+        {
+            panel.SetActive(panel == _selfPanel);
+        }
     }
 }
